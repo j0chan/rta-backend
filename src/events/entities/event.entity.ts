4a18653ebd4394b72eebdc69f6a2cf75406fc796
Youngchan <wdc4512@naver.com>
2025-02-24 12:53:20 +0900

    feat: Event엔터티에 TypeORM 적용

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,12 +1,28 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
 import { EventStatus } from "./event-status.enum";
 
 export class Event {
+    @PrimaryGeneratedColumn()
     event_id: number;
+
+    @Column()
     store_id: number;
+
+    @Column()
     title: string;
+    
+    @Column()
     description: string;
+    
+    @Column()
     start_date: Date;
+    
+    @Column()
     end_date: Date;
+    
+    @Column()
     event_status: EventStatus;
+    
+    @CreateDateColumn({type: "timestamp"})
     created_at: Date;
 }
