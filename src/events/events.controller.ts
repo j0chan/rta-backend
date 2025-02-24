9933342ffb189a55c6bdf389f3b4842da13ec41a
Youngchan <wdc4512@naver.com>
2025-02-24 14:39:27 +0900

    feat: 이벤트 등록 기능[CREATE] 구현

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { CreateEventRequestDto } from './dto/create-event-request.dto';
+import { ApiResponseDto } from 'src/common/api-reponse-dto/api-response.dto';
 
-@Controller('events')
-export class EventsController {}
+@Controller('api/events')
+export class EventsController {
+
+    // 생성자 정의
+    constructor(private eventsService: EventsService) { }
+
+    // CREATE
+    // 구현: requestDto, responseDto, promise/async
+    // 미구현: logger
+    @Post('/')
+    async createEvent(@Body() createEventRequestDto: CreateEventRequestDto): Promise<ApiResponseDto<Event>> {
+        await this.eventsService.createEvent(createEventRequestDto)
+        return new ApiResponseDto(true, HttpStatus.CREATED, 'Event Created Successfully!')
+    }
+
+
+    // READ
+
+
+    // UPDATE
+
+
+    // DELETE
+
+}
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Event } from './entities/event.entity';
+import { Repository } from 'typeorm';
+import { CreateEventRequestDto } from './dto/create-event-request.dto';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+
+    // Event 엔터티 주입
+    constructor(
+        @InjectRepository(Event)
+        private eventRepository: Repository<Event>
+    ) { }
+
+    // CREATE
+    // 구현: requestDto, responseDto, promise/async
+    // 미구현: logger, 에러 처리
+    // 비고: 임시 시간값, 임시 스토어 id 사용
+    async createEvent(createEventRequestDto: CreateEventRequestDto): Promise<Event> {
+        const { store_id, title, description, start_date, end_date } = createEventRequestDto
+
+        // DTO에서 받은 값을 `Date` 객체로 변환, end_date가 명시되지 않았다면 7일 뒤로 설정
+        const startDate = start_date ? new Date(start_date) : new Date();
+        const endDate = end_date ? new Date(end_date) : new Date(startDate);
+        if (!end_date) {
+            endDate.setDate(endDate.getDate() + 7);
+        }
+
+        // 임시 스토어 id
+        const tempStoreId: number = 1
+
+        const newEvent: Event = this.eventRepository.create({
+            store_id: tempStoreId,
+            title,
+            description,
+            start_date: startDate,
+            end_date: endDate,
+        })
+        const createdEvent = await this.eventRepository.save(newEvent)
+
+        return createdEvent;
+    }
+}
