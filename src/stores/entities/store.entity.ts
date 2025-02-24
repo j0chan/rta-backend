50859cdc50d89e3e0935a8c6468e90c75e5b8df8
3405000 <jaeeunnooh@gmail.com>
2025-02-24 16:39:19 +0900

    chore: store typeorm decorator 추가

--- src/stores/entities/store.entity.ts
+++ src/stores/entities/store.entity.ts
@@ -1,15 +1,39 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
 import { StoreCategory } from "./store-category.enum"
+import { User } from "src/users/entities/user.entity"
 
+@Entity()
 export class Store {
+    @PrimaryGeneratedColumn()
     store_id: number
+
+    @ManyToOne(() => User, (user) => user.user_id)
     user_id: number
+
+    @Column({ nullable: false })
     store_name: string
+
+    @Column()
     owner_name: string
+
+    @Column()
     category: StoreCategory
+
+    @Column()
     address: string
+
+    @Column()
     latitude: number
+
+    @Column()
     longtitude: number
+
+    @Column()
     contact_number: string
+
+    @Column()
     description: string
+
+    @CreateDateColumn({ type: 'timestamp' })
     created_at: Date
 }
