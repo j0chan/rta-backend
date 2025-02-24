fac28f7e3c9e519b5235eb4d216942051153cd5c
Youngchan <wdc4512@naver.com>
2025-02-24 14:38:10 +0900

    feat: EventStatus필드 기본값 설정

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { EventStatus } from "./event-status.enum";
 
+@Entity()
 export class Event {
     @PrimaryGeneratedColumn()
     event_id: number;
@@ -21,7 +22,7 @@ export class Event {
     end_date: Date;
     
     @Column()
-    event_status: EventStatus;
+    event_status: EventStatus = EventStatus.ONGOING;
     
     @CreateDateColumn({type: "timestamp"})
     created_at: Date;
