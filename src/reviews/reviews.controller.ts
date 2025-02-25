b53dcb223c576d8b8005f325b521515b5bd9edd7
Youngchan <wdc4512@naver.com>
2025-02-25 18:19:34 +0900

    feat: 리뷰 등록 기능[CREATE] 구현

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common'
+import { ApiResponseDto } from 'src/common/api-reponse-dto/api-response.dto'
+import { ReviewsService } from './reviews.service'
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common'
+import { CreateReviewRequestDTO } from './DTO/create-review-request.dto'
 
-@Controller('reviews')
-export class ReviewsController {}
+@Controller('api/reviews')
+export class ReviewsController {
+
+    // 생성자 정의
+    constructor(private reviewsService: ReviewsService) { }
+
+    // CREATE
+    // 미구현: logger
+    @Post('/')
+    async createReview(@Body() createReviewRequestDTO: CreateReviewRequestDTO): Promise<ApiResponseDto<Event>> {
+        await this.reviewsService.createReview(createReviewRequestDTO)
+        return new ApiResponseDto(true, HttpStatus.CREATED, 'Review Created Successfully!')
+    }
+
+
+    // READ
+    // 미구현: logger
+
+
+
+    // UPDATE
+    // 미구현: logger
+
+
+
+    // DELETE
+    // 미구현: logger
+}
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from './entites/review.entity';
+import { Repository } from 'typeorm';
+import { CreateReviewRequestDTO } from './DTO/create-review-request.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+
+    // Review 엔터티 주입
+    constructor(
+        @InjectRepository(Review)
+        private reviewRepository: Repository<Review>
+    ) { }
+
+    // CREATE
+    // 미구현: logger, 에러 처리
+    // 비고: store_id, user_id, keywords는 원래 DTO로 전달해야 한다. 지금은 안되므로 임시값 사용.
+    async createReview(createReviewRequestDTO: CreateReviewRequestDTO): Promise<Review> {
+        const { store_id, user_id, content, keywords } = createReviewRequestDTO
+
+        // 임시 store_id
+        const tempStoreId: number = 1
+
+        // 임시 user_id
+        const tempUserId: number = 2
+
+        // 임시 AI 요약 키워드
+        const tempKeywords: string = "임시 AI 요약 키워드들3"
+
+
+        const newReview: Review = this.reviewRepository.create({
+            store_id: tempStoreId,
+            user_id: tempUserId,
+            content: content,
+            keywords: tempKeywords,
+            created_at: new Date(),
+            updated_at: null
+        })
+
+        const createdEvent: Review = await this.reviewRepository.save(newReview)
+
+        return createdEvent
+    }
+}
