44eb426f214c21095d0cc17dd5b0e99ddb792138
Youngchan <wdc4512@naver.com>
2025-02-25 19:06:22 +0900

    feat: ReadAllReviewsRequestDTO 생성

---
+++ src/reviews/DTO/read-all-reviews-request.dto.ts
@@ -0,0 +1,32 @@
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Review } from "../entites/review.entity";
+
+export class ReadAllReviewsRequestDTO {
+    @IsNotEmpty()
+    @IsNumber()
+    user_id: number
+
+    @IsNotEmpty()
+    @IsString()
+    content: string
+
+    @IsNotEmpty()
+    @IsString()
+    keywords: string
+
+    @IsNotEmpty()
+    @IsDate()
+    created_at: Date
+
+    @IsNotEmpty()
+    @IsBoolean()
+    isModified: boolean
+
+    constructor(review: Review) {
+        this.user_id = review.user_id
+        this.content = review.content
+        this.keywords = review.keywords
+        this.created_at = review.created_at
+        this.isModified = review.isModified
+    }
+}
