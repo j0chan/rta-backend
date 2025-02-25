d98b7882ac324e2c3157a9d4cc9f5d4ae16f416c
Youngchan <wdc4512@naver.com>
2025-02-25 18:19:03 +0900

    refactor: Review 엔터티에 TypeORM 적용 및 최적화

--- src/reviews/entites/review.entity.ts
+++ src/reviews/entites/review.entity.ts
@@ -1,10 +1,29 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
 export class Review {
+    @PrimaryGeneratedColumn()
     review_id: number
+    
+    @Column()
     store_id: number
+    
+    @Column()
     user_id: number
+    
+    @Column()
     content: string
+    
+    @Column()
     keywords: string
+    
+    @CreateDateColumn({ type: "timestamp" })
     created_at: Date
-    updated_at: Date
-    isModified: boolean
+    
+    @UpdateDateColumn({ type: "timestamp", nullable: true })
+    // 기본적으로 null값, 수정되었다면 Date 변경
+    updated_at: Date | null;
+
+    @Column()
+    isModified: boolean = false
 }
