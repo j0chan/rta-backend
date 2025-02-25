a288feca5ab9996f2ac0eb31f85532737fff498f
CHJ <huihee@g.hongik.ac.kr>
2025-02-25 11:48:44 +0900

    chore: 코드 컨벤션 적용

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,12 +1,12 @@
-import { EventStatus } from "./event-status.enum";
+import { EventStatus } from "./event-status.enum"
 
 export class StoreEvent {
-    event_id: number;
-    store_id: number;
-    title: string;
-    description: string;
-    start_date: Date;
-    end_date: Date;
-    event_status: EventStatus;
-    created_at: Date;
+    event_id: number
+    store_id: number
+    title: string
+    description: string
+    start_date: Date
+    end_date: Date
+    event_status: EventStatus
+    created_at: Date
 }
