0b4695afe372d9731e3ab58aa6917feabeb92338
Youngchan <wdc4512@naver.com>
2025-02-25 18:20:26 +0900

    feat: reviews.module에 컨트롤러, 서비스 등록

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewsService } from './reviews.service';
+import { ReviewsController } from './reviews.controller';
+import { Review } from './entites/review.entity';
 
-@Module({})
-export class ReviewsModule {}
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Review])
+    ],
+    providers: [ReviewsService],
+    controllers: [ReviewsController],
+})
+export class ReviewsModule { }
