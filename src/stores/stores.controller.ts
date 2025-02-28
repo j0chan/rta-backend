3080da783bbcbd2a858f7715ed5bbfe768387f34
3405000 <jaeeunnooh@gmail.com>
2025-02-28 16:29:45 +0900

    feat: store request CRUD 구현

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -1,15 +1,64 @@
-import { Body, Controller, Get, NotFoundException, Param, Patch, Post, Put, Query } from '@nestjs/common'
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Put, Query } from '@nestjs/common'
 import { StoresService } from './stores.service'
 import { CreateStoreDTO } from './DTO/create-store.dto'
 import { ReadStoreDTO } from './DTO/read-store.dto'
 import { ReadStoreAddressDTO } from './DTO/read-store-address.dto'
 import { StoreCategory } from './entities/store-category.enum'
 import { UpdateStoreDetailDTO } from './DTO/update-store-detail.dto'
+import { CreateStoreRequestDTO } from './DTO/create-store-request.dto'
+import { ReadStoreRequestDTO } from './DTO/read-store-request.dto'
+import { UpdateStoreRequestDTO } from './DTO/update-store-request.dto'
 
 @Controller('api/stores')
 export class StoresController {
     constructor(private storesService: StoresService) { }
 
+
+    // --------------------
+    // Store Request 가게 신청서
+
+    // CREATE
+    // 가게 신청서 생성
+    @Post('/request/')
+    async createStoreRequest(@Body() createStoreRequestDTO: CreateStoreRequestDTO): Promise<void> {
+        await this.storesService.createStoreRequest(createStoreRequestDTO)
+    }
+
+    // READ
+    // 모든 가게 신청서 조회 (관리자 전용)
+    @Get('/request/')
+    async getAllStoreRequest(): Promise<ReadStoreRequestDTO[]> {
+        const storeRequests = await this.storesService.getAllStoreRequest()
+        if (!storeRequests) { throw new Error('No storeRequests found') }
+
+        const readStoreRequestDTO = storeRequests.map(storeRequest => new ReadStoreRequestDTO(storeRequest))
+        return readStoreRequestDTO
+    }
+
+    // 특정 가게 신청서 조회
+    @Get('/request/:request_id')
+    async getStoreRequestById(@Param('request_id') request_id: number): Promise<ReadStoreRequestDTO> {
+        const storeRequest = await this.storesService.getStoreRequestById(request_id)
+        if (!storeRequest) { throw new Error(`StoreRequest with ID ${request_id} not found`)}
+        
+        return new ReadStoreRequestDTO(storeRequest)
+    }
+
+    // UPDATE
+    // 가게 신청서 처리 (관리자 전용)
+    @Put('/request/:request_id')
+    async updateStoreRequest(@Param('request_id') request_id: number, @Body() updateStoreRequestDTO: UpdateStoreRequestDTO) {
+        await this.storesService.updateStoreRequest(request_id, updateStoreRequestDTO)
+    }
+
+    // DELETE
+    // 가게 신청서 삭제
+    @Delete('/request/:request_id')
+    async deleteStoreRequest(@Param('request_id') request_id: number) {
+        await this.storesService.deleteStoreRequest(request_id)
+    }
+
+    // --------------------
     // CREATE
     // 새로운 가게 생성하기
     @Post('/')
@@ -50,7 +99,7 @@ export class StoresController {
         return new ReadStoreAddressDTO(store)
     }
     
-    // 가게 업종으로 검색(필터링) 조회 -- 안됨
+    // 가게 업종으로 검색(필터링) 조회
     @Get('/')
     async getStoresByCategory(@Query('category') category: StoreCategory): Promise<ReadStoreDTO[]> {
         const stores = await this.storesService.getStoresByCategory(category)
@@ -60,7 +109,7 @@ export class StoresController {
     }
 
     // UPDATE
-    // 가게 매니저 속성 수정 (관리자 전용)
+    // 가게 매니저 수정 (관리자 전용)
     @Patch('/:store_id')
     async updateStoreManager(@Param('store_id') store_id: number, @Body('user_id') user_id: number): Promise<void> {
         await this.storesService.updateStoreManager(store_id, user_id)
@@ -72,4 +121,8 @@ export class StoresController {
         
         await this.storesService.updateStoreDetail(store_id, updateStoreDetailDTO)
     }
+
+    // DELETE
+
+    
 }
--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { StoresService } from './stores.service'
 import { StoresController } from './stores.controller'
 import { Store } from './entities/store.entity'
+import { StoreRequest } from './entities/store-request.entity'
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Store])
+        TypeOrmModule.forFeature([Store, StoreRequest])
     ],
     providers: [StoresService],
     controllers: [StoresController],
