7fdbb38fdc090d2cccbd5faa76f04d642cb0f42f
Youngchan <wdc4512@naver.com>
2025-02-28 17:18:33 +0900

    feat: TypeORM을 위한 ReviewReply 엔터티 등록

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -4,10 +4,11 @@ import { ReviewsService } from './reviews.service'
 import { ReviewsController } from './reviews.controller'
 import { Review } from './entites/review.entity'
 import { StoresModule } from 'src/stores/stores.module'
+import { ReviewReply } from './entites/review-reply.entity'
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Review]),
+        TypeOrmModule.forFeature([Review, ReviewReply]),
         StoresModule,
     ],
     providers: [ReviewsService],
