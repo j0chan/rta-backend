25b7aa4b727b36fd03398f6205f1ccbbeea906f5
Youngchan <wdc4512@naver.com>
2025-03-01 16:24:56 +0900

    refactor: 코드 컨벤션 정리

--- src/review-replies/entities/review-reply.entity.ts
+++ src/review-replies/entities/review-reply.entity.ts
@@ -10,7 +10,7 @@ export class ReviewReply {
     // 댓글 삭제 시 대댓글 자동 삭제
     @OneToOne(() => Review, (review) => review.reply, { onDelete: 'CASCADE' })
     @JoinColumn({name:"review_id"})
-    review: Review;
+    review: Review
 
     @Column()
     content: string
