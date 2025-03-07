d16c8caf8ac2c7c8c1823e56fa07b59890ab938b
Youngchan <wdc4512@naver.com>
2025-03-07 15:32:33 +0900

    fix: 컨벤션 변경에 따른 menus폴더 오류 수정

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,4 +1,4 @@
-import { ApiResponseDto } from 'src/common/api-reponse-dto/api-response.dto';
+import { ApiResponseDTO } from 'src/common/api-reponse-dto/api-response.dto';
 import { CreateMenuDTO } from './DTO/create-menu.dto';
 import { MenusService } from './menus.service';
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put } from '@nestjs/common';
@@ -14,29 +14,29 @@ export class MenusController {
     // CREATE - 새로운 메뉴 등록
     // 미구현: logger
     @Post('/')
-    async createMenu(@Body() createMenuDTO: CreateMenuDTO): Promise<ApiResponseDto<Menu>> {
+    async createMenu(@Body() createMenuDTO: CreateMenuDTO): Promise<ApiResponseDTO<Menu>> {
         await this.menusService.createMenu(createMenuDTO)
-        return new ApiResponseDto(true, HttpStatus.CREATED, 'Menu Registered Successfully!')
+        return new ApiResponseDTO(true, HttpStatus.CREATED, 'Menu Registered Successfully!')
 
     }
 
     // READ[1] - 해당 가게 모든 메뉴 조회
     // 미구현: logger
     @Get('/')
-    async readAllMenus(): Promise<ApiResponseDto<ReadAllMenusDTO[]>> {
+    async readAllMenus(): Promise<ApiResponseDTO<ReadAllMenusDTO[]>> {
         const menus: Menu[] = await this.menusService.readAllMenus()
         const readAllMenusDTO = menus.map(menu => new ReadAllMenusDTO(menu))
 
-        return new ApiResponseDto(true, HttpStatus.OK, 'Successfully Retrieved All Menus!', readAllMenusDTO)
+        return new ApiResponseDTO(true, HttpStatus.OK, 'Successfully Retrieved All Menus!', readAllMenusDTO)
     }
 
     // READ[2] - 해당 가게 특정 메뉴 조회
     // 미구현: logger
     @Get('/:menu_id')
-    async readMenuById(@Param('menu_id') menu_id: number): Promise<ApiResponseDto<ReadMenuDTO>> {
+    async readMenuById(@Param('menu_id') menu_id: number): Promise<ApiResponseDTO<ReadMenuDTO>> {
         const foundMenu: Menu = await this.menusService.readMenuById(menu_id)
 
-        return new ApiResponseDto(true, HttpStatus.OK, 'Successfully Retrieved Event!', new ReadMenuDTO(foundMenu))
+        return new ApiResponseDTO(true, HttpStatus.OK, 'Successfully Retrieved Event!', new ReadMenuDTO(foundMenu))
     }
 
     // UPDATE - by menu_id
@@ -44,16 +44,16 @@ export class MenusController {
     @Put('/:menu_id')
     async updateMenuById(
         @Param('menu_id') menu_id: number,
-        @Body() updateMenuDTO: UpdateMenuDTO): Promise<ApiResponseDto<void>> {
+        @Body() updateMenuDTO: UpdateMenuDTO): Promise<ApiResponseDTO<void>> {
         await this.menusService.updateMenuById(menu_id, updateMenuDTO)
-        return new ApiResponseDto(true, HttpStatus.NO_CONTENT, 'Menu Updated Successfully!')
+        return new ApiResponseDTO(true, HttpStatus.NO_CONTENT, 'Menu Updated Successfully!')
     }
 
     // DELETE - by menu_id
     // 미구현: logger
     @Delete('/:menu_id')
-    async deleteMenuById(@Param('menu_id') menu_id: number): Promise<ApiResponseDto<void>> {
+    async deleteMenuById(@Param('menu_id') menu_id: number): Promise<ApiResponseDTO<void>> {
         await this.menusService.deleteMenuById(menu_id)
-        return new ApiResponseDto(true, HttpStatus.NO_CONTENT, 'Menu Deleted Successfully!')
+        return new ApiResponseDTO(true, HttpStatus.NO_CONTENT, 'Menu Deleted Successfully!')
     }
 }
