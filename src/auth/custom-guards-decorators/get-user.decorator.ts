ed25b7f1caab0cc50fcff2bc7dec30ded23932dc
Youngchan <wdc4512@naver.com>
2025-03-09 17:33:09 +0900

    refactor: 코드 컨벤션 정리

--- src/auth/custom-guards-decorators/get-user.decorator.ts
+++ src/auth/custom-guards-decorators/get-user.decorator.ts
@@ -1,6 +1,6 @@
-import { User } from 'src/users/entities/user.entity';
-import { ExecutionContext } from './../../../node_modules/@nestjs/common/interfaces/features/execution-context.interface.d';
-import { createParamDecorator } from "@nestjs/common";
+import { User } from 'src/users/entities/user.entity'
+import { ExecutionContext } from './../../../node_modules/@nestjs/common/interfaces/features/execution-context.interface.d'
+import { createParamDecorator } from "@nestjs/common"
 
 export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest()
