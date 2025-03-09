64365f8a4095f20a42f77fc93d868d50df3a682c
Youngchan <wdc4512@naver.com>
2025-03-09 16:51:52 +0900

    fix: JwtStrategy를 provider로 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,25 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UsersModule } from 'src/users/users.module'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
-
+import { JwtStrategy } from './strategies/jwt.strategy'
 @Module({
     imports: [
         UsersModule,
         PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.register({
             secret: process.env.JWT_SECRET,
-            signOptions:{
+            signOptions: {
                 expiresIn: process.env.JWT_EXPIRATION
             }
         }),
     ],
-    providers: [AuthService],
+    providers: [
+        AuthService,
+        JwtStrategy
+    ],
     controllers: [AuthController],
     exports: [AuthService],
 })
