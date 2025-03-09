68e39395c48fbe36e4c26c27312618335ace2704
Youngchan <wdc4512@naver.com>
2025-03-09 11:53:44 +0900

    chore: Bcrypt, JWT 의존성 설치

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common'
-import { UsersService } from 'src/users/users.service'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { UsersModule } from 'src/users/users.module'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
     imports: [
-        UsersService
+        UsersModule,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions:{
+                expiresIn: process.env.JWT_EXPIRATION
+            }
+        }),
     ],
     providers: [AuthService],
     controllers: [AuthController],
