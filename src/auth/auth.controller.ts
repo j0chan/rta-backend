7613311ea1cc4563c3809179be237cd12a25465e
Youngchan <wdc4512@naver.com>
2025-03-09 12:20:44 +0900

    feat: JWT를 이용한 회원가입/로그인 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common'
+import { AuthService } from './auth.service';
+import { CreateUserDTO } from 'src/users/DTO/create-user.dto';
+import { ApiResponseDTO } from 'src/common/api-reponse-dto/api-response.dto';
+import { SignInDTO } from './DTO/sign-in.dto';
+import { Response } from 'express';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+    // init
+    constructor(private authService: AuthService) { }
+
+    // Sign-Up
+    @Post('/signup')
+    async singUp(@Body() createUserDTO: CreateUserDTO): Promise<ApiResponseDTO<void>> {
+        await this.authService.signUp(createUserDTO)
+
+        return new ApiResponseDTO(true, HttpStatus.CREATED, 'User Created Successfully!')
+    }
+
+    // Sign-In
+    @Post('/signin')
+    async singIn(@Body() signInDTO: SignInDTO, @Res() res: Response): Promise<void> {
+        const accessToken = await this.authService.signIn(signInDTO)
+
+        // [2] JWT를 헤더에 저장 후 ApiResponse를 바디에 담아 전송
+        res.setHeader('Authorization', accessToken)
+        const response = new ApiResponseDTO(true, HttpStatus.OK, 'User Logged In Successfully!', { accessToken })
+
+        res.send(response)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { SignInDTO } from './DTO/sign-in.dto';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDTO } from './../users/DTO/create-user.dto';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
+import { error } from 'console';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    // init
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    // Sign-Up
+    async signUp(createUserDTO: CreateUserDTO): Promise<void> {
+        const { email, password, nickname, phone_number, role } = createUserDTO
+        if (!email || !password || !nickname || !phone_number || !role) {
+            throw new BadRequestException('Something Went Wrong!')
+        }
+
+        await this.usersService.checkEmailExist(email)
+
+        const hashedPassword = await this.hashPassword(password)
+
+        await this.usersService.createUser({
+            email,
+            password: hashedPassword,
+            nickname,
+            phone_number,
+            role,
+        })
+    }
+
+    // Sign-In
+    async signIn(signInDTO: SignInDTO): Promise<string> {
+        const { email, password } = signInDTO
+
+        try {
+            const existingUser = await this.usersService.findUserByEmail(email)
+
+            if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
+                throw new UnauthorizedException('Invalid Credentials!')
+            }
+
+            // [1] JWT 토큰 생성
+            const payload = {
+                id: existingUser.user_id,
+                email: existingUser.email,
+                nickname: existingUser.nickname,
+                role: existingUser.role
+            }
+            const accessToken = await this.jwtService.sign(payload)
+
+            return accessToken
+        } catch {
+            throw error
+        }
+    }
+
+    // 비밀번호 암호화(해싱)
+    async hashPassword(password: string): Promise<string> {
+        const salt = await bcrypt.genSalt()
+        return await bcrypt.hash(password, salt)
+    }
+}
