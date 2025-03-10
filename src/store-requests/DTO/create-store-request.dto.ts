07ec9df7e4fa1af7ba9b2416c5fb4fcdaf4bb874
3405000 <jaeeunnooh@gmail.com>
2025-03-10 11:56:47 +0900

    refactor: User <-> StoreRequest 관계 설정

--- src/store-requests/DTO/create-store-request.dto.ts
+++ src/store-requests/DTO/create-store-request.dto.ts
@@ -1,32 +1,11 @@
 import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator"
-import { StoreCategory } from "src/stores/entities/store-category.enum"
 
 export class CreateStoreRequestDTO {
+    @IsNotEmpty()
     @IsNumber()
     user_id: number
 
     @IsNotEmpty()
-    @IsString()
-    store_name: string
-
-    @IsEnum(StoreCategory)
-    category: StoreCategory
-
-    @IsNotEmpty()
-    @IsString()
-    address: string
-
-    @IsNotEmpty()
     @IsNumber()
-    latitude: number
-    
-    @IsNotEmpty()
-    @IsNumber()
-    longitude: number
-
-    @IsString()
-    contact_number: string
-
-    @IsString()
-    description: string
+    store_id: number
 }
