17952642783657ea05fe25cbcdf7a795dd9ad503
Youngchan <wdc4512@naver.com>
2025-03-19 17:04:32 +0900

    feat: 특정 유저가 가진 모든 store 조회 기능 구현

--- src/stores/DTO/read-store.dto.ts
+++ src/stores/DTO/read-store.dto.ts
@@ -1,8 +1,10 @@
+import { User } from "src/users/entities/user.entity"
 import { StoreCategory } from "../entities/store-category.enum"
 import { Store } from "../entities/store.entity"
 
 export class ReadStoreDTO {
     store_id: number
+    user_id: User
     store_name: string
     owner_name: string
     category: StoreCategory
@@ -14,6 +16,7 @@ export class ReadStoreDTO {
 
     constructor(store: Store) {
         this.store_id = store.store_id
+        this.user_id = store.user_id
         this.store_name = store.store_name
         this.owner_name = store.owner_name
         this.category = store.category
