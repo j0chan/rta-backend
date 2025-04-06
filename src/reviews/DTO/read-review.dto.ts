d801c066c955a1b218a93c2765d50c80c875cdc7
Youngchan <wdc4512@naver.com>
2025-04-06 15:36:25 +0900

    feat: read-review-DTO에 user_id 추가

--- src/reviews/DTO/read-review.dto.ts
+++ src/reviews/DTO/read-review.dto.ts
@@ -4,6 +4,7 @@ import { Review } from "../entites/review.entity"
 export class ReadReviewDTO {
     review_id: number
     user_name: string
+    user_id: number
     store_name: string
     content: string
     helpful_count: number
@@ -15,6 +16,7 @@ export class ReadReviewDTO {
     constructor(review: Review) {
         this.review_id = review.review_id
         this.user_name = review.user.nickname
+        this.user_id= review.user.user_id
         this.store_name = review.store.store_name
         this.content = review.content
         this.helpful_count = review.helpful_count
