5dc4395da76a283abcecbeb5120487ceb568ae61
Youngchan <wdc4512@naver.com>
2025-04-06 13:48:00 +0900

    feat: 전역 validationPipe 설정을 통한 @IsNotEmpty() 등의 데코레이터가 작동하도록 설정

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import { AppModule } from './app.module'
 import * as express from 'express'
 import * as path from 'path'
 import * as dotenv from 'dotenv'
+import { ValidationPipe } from '@nestjs/common'
+
 dotenv.config() // 환경변수 load
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule)
 
-  // CORS 설정 추가
+  // 전역 ValidationPipe 설정 추가
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성은 제거
+      forbidNonWhitelisted: true, // DTO에 없는 값이 들어오면 예외 발생
+      transform: true, // 자동 타입 변환 (예: string → number)
+    }),
+  )
+
+  // CORS 설정
   app.enableCors({
     origin: ['http://localhost:8100', 'http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -17,7 +28,7 @@ async function bootstrap() {
   })
 
   // 정적 파일 제공 (public 폴더)
-  app.use('/test', express.static(path.join(__dirname, '..', 'public'))) // 루트까지(..) 이동한 뒤, public을 찾는다.
+  app.use('/test', express.static(path.join(__dirname, '..', 'public')))
 
   await app.listen(process.env.PORT ?? 3000)
 }
