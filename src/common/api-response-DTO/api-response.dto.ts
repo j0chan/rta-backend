a363baeabbc135efc0b1729b9047ece8d6a4daee
j0chan <wdc4512@naver.com>
2025-07-01 18:10:32 +0900

    refactor: api-response.dto 생성

---
+++ src/common/api-response-DTO/api-response.dto.ts
@@ -0,0 +1,15 @@
+export class ApiResponseDTO<T> {
+    success: boolean;
+    statusCode?: number;
+    message?: string;
+    data?: T;
+    error?: string;
+
+    constructor(success: boolean, statusCode?: number, message?: string, data?: T, error?: string) {
+        this.success = success;
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.error = error;
+    }
+}
